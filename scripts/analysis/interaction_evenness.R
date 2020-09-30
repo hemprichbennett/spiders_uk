a070af27160f05d1333f757b527a4f3d54eb4fbc
Dave Hemprich-Bennett <hemprich.bennett@gmail.com>
2020-09-30 16:40:05 +0100

    Erk, renamed file: the analysis is IE not ISA!

--- scripts/analysis/interaction_evenness.R
+++ scripts/analysis/interaction_evenness.R
@@ -64,56 +64,56 @@ for(i in 1:length(nets)){
 }
 
 
-# calculate the real values for isa, and null-model values
+# calculate the real values for IE, and null-model values
 
-isa_vals_list <- list() # list for the output to go into
+IE_vals_list <- list() # list for the output to go into
 it <- 1 # an iterator
 
 for(i in 1:length(copy_number_nets)){
   
   netname <- names(copy_number_nets)[i] # the name of the network in question
-  isa_vals_list[[it]] <- c( netname,
-    networklevel(copy_number_nets[[i]], index = 'ISA'),
+  IE_vals_list[[it]] <- c( netname,
+    networklevel(copy_number_nets[[i]], index = 'interaction evenness'),
     'real')
   it <- it + 1
   
   
   for(j in 1:1000){
       null_val <- permatswap(copy_number_nets[[i]], fixedmar='both',mtype="count",times=1, method="quasiswap")$perm[[1]] %>%
-        networklevel(index = 'ISA')
+        networklevel(index = 'interaction evenness')
       
-      isa_vals_list[[it]] <- c(netname, null_val, 'random')
+      IE_vals_list[[it]] <- c(netname, null_val, 'random')
       
       it <- it + 1
   }
   
   }
 
-isa_tib <- bind_rows(isa_vals_list)
+IE_tib <- bind_rows(IE_vals_list)
 
-colnames(isa_tib) <- c('network', 'isa', 'origin')
+colnames(IE_tib) <- c('network', 'IE', 'origin')
 
-isa_tib$isa <- as.numeric(isa_tib$isa)
+IE_tib$IE <- as.numeric(IE_tib$IE)
 
 
-isa_quartiles <- isa_tib %>% 
+IE_quartiles <- IE_tib %>% 
   filter(origin == 'random') %>%
   group_by(network) %>% 
-  summarise(x = quantile(isa, c(0.05, 0.95)), q = c(0.05, 0.95)) %>%
+  summarise(x = quantile(IE, c(0.05, 0.95)), q = c(0.05, 0.95)) %>%
   rename(value = x, quant = q) %>%
   pivot_wider(names_from = quant, values_from = value)
 
-isa_values_final <- isa_tib %>%
+IE_values_final <- IE_tib %>%
   filter(origin == 'real') %>%
   select(-origin) %>%
-  left_join(isa_quartiles)
+  left_join(IE_quartiles)
 
-isa_plot <- ggplot(isa_values_final, aes(x = network, y = isa)) +
+IE_plot <- ggplot(IE_values_final, aes(x = network, y = IE)) +
   geom_point() +
   geom_errorbar(aes(ymin = `0.05`, ymax = `0.95`))+ 
-  ylab('Interaction strength asymmetry') +
+  ylab('Interaction evenness') +
 #  ylim(c(0,1))+
   theme_bw()
 
-isa_plot  
-ggsave('figures/isa.pdf', isa_plot)
+IE_plot  
+ggsave('figures/interaction_evenness.pdf', IE_plot)
